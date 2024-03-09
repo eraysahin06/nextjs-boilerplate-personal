0d3236866434b1a82d1f7de9cbc797a7a845cfb0
Eray Sahin <eraysahinmoon@gmail.com>
2024-03-09 15:56:35 -0500

    NEXT BOILERPLATE

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import styles from './about.module.css'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div className={styles.container}>
+      <Image src="https://images.pexels.com/photos/6957078/pexels-photo-6957078.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="about image" fill />
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
